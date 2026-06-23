26cc863689aaecb3a3dc1a0636f943c02cfb55c0
talarivinod <tvndkumar@gmail.com>
2026-06-23 10:11:43 +0530

    updateddd
    Signed-off-by: talarivinod <tvndkumar@gmail.com>

--- FeedMill_Backend/app.js
+++ FeedMill_Backend/app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var addstock = require('./routes/addstcok');
 
 var app = express();
 
@@ -21,11 +22,13 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/api/addstock', addstock);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
+app.use('/api/receviedstock',receviedStocks);
 
 // error handler
 app.use(function(err, req, res, next) {
