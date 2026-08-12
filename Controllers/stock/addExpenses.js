781d140953b19c17daf125dc5dec49bb3e7a34ca
talarivinod <tvndkumar@gmail.com>
2026-08-12 18:00:01 +0530

    updatedd
    Signed-off-by: talarivinod <tvndkumar@gmail.com>

--- Controllers/stock/addExpenses.js
+++ Controllers/stock/addExpenses.js
@@ -4,11 +4,11 @@
 const ExpenseModel = require('../../app/Models/expenses.js');
 
 const addPurchasedStock = async (req, res) => {
-// Add Expense
+    // Add Expense
     try {
 
 
-          if (!req.body || !req.body.expensesData) {
+        if (!req.body || !req.body.expensesData) {
             console.log("daeme", req.body.expensesData)
             return res.status(400).json({
                 message: "expensesData is missing in request"
@@ -42,31 +42,45 @@ const addPurchasedStock = async (req, res) => {
 
         if (amount <= 0) {
             return res.status(200).json({
-                 response: 0,
+                response: 0,
                 message: 'Amount must be greater than 0'
             });
+        }
+        var   filedbpath = ""
+        if (req.files && req.files.image) {
+            const file = req.files.image;
+            const imageid = "exp@" + idb.GenerateIDS(9);
+            const filename = imageid + file.name;
+            const filemvpath = `./public/images/expenses/${filename}`;
+            const filedbpath = `/images/expenses/${filename}`;
+
+            // MOVE FILE (PROMISE SAFE)
+            await new Promise((resolve, reject) => {
+                file.mv(filemvpath, err => err ? reject(err) : resolve());
+            });
+
         }
 
         // TODO: Save to database
         const expense = await ExpenseModel.create({
-          stockPointName,
-          expenseCategory,
-          amount,
-          expenseDate,
-          description,
-          attachment,
+            stockPointName,
+            expenseCategory,
+            amount,
+            expenseDate,
+            description,
+            attachment:filedbpath,
         });
 
         res.status(200).json({
-              response: 3,
+            response: 3,
             message: 'Expense added successfully',
             data: {
-                stockPoint,
+                stockPointName,
                 expenseCategory,
                 amount,
                 expenseDate,
                 description,
-                attachment
+                filedbpath
 
             }
         });
